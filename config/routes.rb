ef56b930ebf2a77a189b623cf5371ef5f6bf7152
Hatem <hatem.deux@gmail.com>
2020-05-04 21:51:49 -0400

    implements User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,17 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  
+  # resources :sessions, only: [:new, :create, :destroy]
+  # resources :users, only: [:new, :create]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
 
   namespace :admin do
     root to: 'dashboard#show'
